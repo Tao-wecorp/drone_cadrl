c5c5e771b720a9e0abd75e96fe08c4c44989b1a3
boringtao <xinghui.tao@wecorpindustries.com>
2020-05-24 08:36:09 +0100

    Mod: env

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
-sudo apt-get install python-pip 
+sudo apt-get install python-pip
+sudo apt-get install ffmpeg freeglut3-dev xvfb
 sudo apt-get install ros-melodic-ros-control ros-melodic-ros-controllers ros-melodic-joint-state-publisher-gui ros-melodic-teleop-twist-keyboard ffmpeg -y
 
 pip3 install imageio==2.6.1 tensorflow-gpu==1.15.2 keras==2.3.1 pyglet==1.3.0 keras-rl==0.4.2
-pip3 install stable-baselines[mpi]==2.10.0 torch==1.5.0 torchvision==0.6.0 gym==0.17.2
+pip3 install stable-baselines[mpi]==2.10.0 torch==1.5.0 torchvision==0.6.0 gym==0.17.2 stable-baselines3 optuna
 pip3 uninstall opencv-python==4.2.0.34
 
 pip freeze > requirements.txt
