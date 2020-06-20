ac331947c082f878031f71d62fbea2a7e393b7b0
boringtao <xinghui.tao@wecorpindustries.com>
2020-06-20 15:49:03 +0100

    Mod: yaw task setup

--- src/drone_openai/envs/yaw_task.py
+++ src/drone_openai/envs/yaw_task.py
@@ -31,24 +31,20 @@ class SJTUYawEnv(yaw_env.SJTUDroneEnv):
         self.init_linear_speed_vector.y = 0
         self.init_linear_speed_vector.z = 0
 
-        self.desired_orientation = Point()
-        self.desired_orientation.x = 0
-        self.desired_orientation.y = 0
-        self.desired_orientation.z = 5
+        self.desired_yaw = 45
 
         # Actions and Observations
-        number_actions = 2
+        number_actions = 3
         self.action_space = spaces.Discrete(number_actions)
 
-        self.max_yaw = 1.57
+        self.max_yaw = 90
         high = numpy.array([self.max_yaw])                                       
         low = numpy.array([-1*self.max_yaw])
         self.observation_space = spaces.Box(low, high)
 
         # Rewards
         self.reward_range = (-numpy.inf, numpy.inf)
-        self.closer_to_pose_reward = 0.1
-        self.end_episode_points = 10
+        self.end_episode_points = 1000
         self.cumulated_steps = 0.0
 
         super(SJTUYawEnv, self).__init__()
@@ -68,11 +64,13 @@ class SJTUYawEnv(yaw_env.SJTUDroneEnv):
         rospy.logdebug("Action "+str(action))
         angular_speed = 0.0
         
-        if action == 0: #Yaw Left
-            angular_speed = self.angular_turn_speed
-        elif action == 1: #Yaw Right
+        if action == 0: #Stay
+            angular_speed = 0
+        elif action == 1: #Yaw Left
+            angular_speed = self.angular_turn_speed 
+        elif action == 2: #Yaw Right
             angular_speed = -1*self.angular_turn_speed
-
+            
         self.move_base(self.init_linear_speed_vector,
                         angular_speed,
                         epsilon=0.05,
@@ -80,35 +78,29 @@ class SJTUYawEnv(yaw_env.SJTUDroneEnv):
 
     def _get_obs(self):
         gt_pose = self.get_gt_pose()
-
         roll, pitch, yaw = self.get_orientation_euler(gt_pose.orientation)
-              
         observations = numpy.array([round(yaw,1)])
         rospy.logdebug("Observations "+str(observations))
+
         return observations
 
     def _is_done(self, observations):
         episode_done = False
 
-        print(observations)
-
-        yaw = observations[0]
-
-        is_inside_workspace_now = self.is_inside_workspace(yaw)
-        has_reached_des_point = self.is_in_desired_position(yaw)
-
+        is_inside_workspace_now = self.is_inside_workspace(observations[0])
+        has_reached_des_point = self.is_in_desired_position(observations[0])
         episode_done = bool(not(is_inside_workspace_now) or has_reached_des_point)
 
         return episode_done
 
     def _compute_reward(self, observations, done):
-
         yaw = observations[0]
 
-        if self.is_in_desired_position(yaw):
+        if self.is_in_desired_position(observations[0]):
             reward = self.end_episode_points
         else:
-            reward = -1*self.end_episode_points
+            distance = self.desired_yaw-observations[0]
+            reward = (1 - abs(distance/self.desired_yaw)) * 100.0
 
         self.cumulated_reward += reward
         self.cumulated_steps += 1
@@ -116,20 +108,19 @@ class SJTUYawEnv(yaw_env.SJTUDroneEnv):
         return reward
 
     
-    def is_in_desired_position(self, yaw_current, epsilon=0.5):
+    def is_in_desired_position(self, yaw_current, epsilon=1):
         is_in_desired_pos = False
 
-        yaw_plus = self.desired_orientation.z + epsilon
-        yaw_minus = self.desired_orientation.z - epsilon
-
+        yaw_plus = self.desired_yaw + epsilon
+        yaw_minus = self.desired_yaw - epsilon
         is_in_desired_pos = bool((yaw_current <= yaw_plus) and (yaw_current > yaw_minus))
 
         return is_in_desired_pos
 
-    def is_inside_workspace(self, yaw):
+    def is_inside_workspace(self, yaw_current):
         is_inside = False
 
-        is_inside = bool((yaw > -1*self.max_yaw) and (yaw <= self.max_yaw))
+        is_inside = bool((yaw_current > -1*self.max_yaw) and (yaw_current <= self.max_yaw))
 
         return is_inside
 
