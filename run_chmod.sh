37a90d9c95303ffa3d9f0aa0aed4cfeb107554ef
boringtao <xinghui.tao@wecorpindustries.com>
2020-06-22 16:03:33 +0100

    Add: zoo

--- run_chmod.sh
+++ run_chmod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/robotics/Workspace/drone_openai/src/drone_openai/envs
+cd /home/robotics/Workspace/drone_openai/src/drone_openai/envs/zoo
 chmod +x *.py
-cd ../../../..
+cd ../../../../..
