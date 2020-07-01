188e3efa5821152d2586c67993f0e2715ecd1be9
boringtao <xinghui.tao@wecorpindustries.com>
2020-07-01 19:56:09 +0100

    Add: teleop twist joy

--- run_script.sh
+++ run_script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 source devel/setup.bash
-rosrun drone_openai cartpole_ppo2.py
+# rosrun drone_openai cartpole_ppo2.py
 # rosrun drone_openai sjtu_train_dqn.py
 # rosrun drone_openai sjtu_train_ppo2_tb.py
 # rosrun drone_openai sjtu_eval.py
 # rosrun drone_openai yaw_task.py
+rosrun drone_openai yaw_state.py
