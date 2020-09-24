caa1d54614e4d28a96cfab38e83c44cb6e520a79
boringtao <xinghui.tao@wecorpindustries.com>
2020-09-24 19:39:26 +0100

    Init: dqn

--- src/drone_openai/envs/goto_dqn.py
+++ src/drone_openai/envs/goto_dqn.py
@@ -10,7 +10,6 @@ from utils.saved_dir import model_dir, log_dir
 
 import parrotdrone_goto
 from stable_baselines import PPO2
-from stable_baselines.common.cmd_util import make_vec_env
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.evaluation import evaluate_policy
 from stable_baselines import results_plotter
@@ -20,12 +19,14 @@ from stable_baselines.common.callbacks import EvalCallback, StopTrainingOnReward
 
 def main():
     rospy.init_node('train_node', anonymous=True)
-    env_id = "ParrotDroneGoto-v0"
-    num_cpu = 36
+    env = gym.make("ParrotDroneGoto-v0")
+    env = Monitor(env, log_dir)
 
-    env = make_vec_env(env_id, n_envs=num_cpu, seed=0)
+    callback_on_best = StopTrainingOnRewardThreshold(reward_threshold=10, verbose=1)
+    eval_callback = EvalCallback(env, callback_on_new_best=callback_on_best, verbose=1)
+
     model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=log_dir)
-    model.learn(total_timesteps=20000, reset_num_timesteps=False)
+    model.learn(total_timesteps=40000, callback=eval_callback, reset_num_timesteps=False)
 
     model.save(model_dir + "goto_ppo2")
 
